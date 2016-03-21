aa937a18dd20bdd97dc17912f630ea332cd92039
Tyler <tyler@digitalinsurancegroup.com>
2016-03-21 17:31:22 -0400

    Updates for machine

--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ var common = require("./common")();
 var user = require("./userFunctions");
 var item = require("./itemFunctions");
 
+var machineSockets = {};
+
 var server = net.createServer(function(socket){
 	console.log("Client connected");
 	socket.setEncoding('utf8');
@@ -19,20 +21,26 @@ var server = net.createServer(function(socket){
 	});
 
 	socket.on("end", function(){
-		console.log("Client left :(");
+		if(!socket.isHttp && socket.identifer != null)
+		{
+			console.log(socket.identifer + " disconnected.");
+			delete machineSockets[socket.identifer];
+		}
 	});
 });
 
 server.listen(8888, "0.0.0.0");
 
+console.log("Server is running on 8888");
+
 var processInput = function(data, socket){
 	data = data.substring(0, data.length - (data.length - data.indexOf(common.END)) + 1);
-	var isHttp = false;
+	socket.isHttp = false;
 	
 	//Check if we have a post request.
 	if(data.substring(0,4).indexOf("POST") > -1)
 	{
-		isHttp = true;
+		socket.isHttp = true;
 
 		//check if we have windows line common.ENDings or unix
 		//and extract the post body
@@ -70,23 +78,23 @@ var processInput = function(data, socket){
 			// 	message: "'data' is required."
 			// }, common.HttpCode.OK);
 		}
-		else if (json.operation.indexOf("user_") > -1 && isHttp)
+		else if (json.operation.indexOf("user_") > -1 && socket.isHttp)
 		{
 			if(json.operation == "user_basic")
 			{
-				user.viewbasic(json.data, isHttp, socket);
+				user.viewbasic(json.data, socket);
 			}
 			else if(json.operation == "user_all")
 			{
-				user.viewall(json.data, isHttp, socket);
+				user.viewall(json.data, socket);
 			}
 			else if(json.operation == "user_create" || json.operation == "user_add")
 			{
-				user.create(json.data, isHttp, socket);
+				user.create(json.data, socket);
 			}
 			else if(json.operation == "user_update")
 			{
-				user.update(json.data, isHttp, socket);
+				user.update(json.data, socket);
 			}
 			else
 			{
@@ -95,20 +103,37 @@ var processInput = function(data, socket){
 			}
 
 		}
-		else if (json.operation.indexOf("item_") > -1 && isHttp)
+		else if (json.operation.indexOf("item_") > -1 && socket.isHttp)
 		{
 			if (json.operation == "item_all")
 			{
-				item.viewall(json.data, isHttp, socket);
+				item.viewall(json.data, socket);
 			}
 			else if (json.operation == "item_purchase")
 			{
-				item.purchase(json.data, isHttp, socket);
+				item.purchase(json.data, socket);
 			}
 			else
 			{
 				response.success = false;
 				response.message = "Function not available";
+			}
+		}
+		else if(json.operation.indexOf("machine_") > -1 && !socket.isHttp)
+		{
+			if(json.operation == "registration")
+			{
+				if(json.data.identifer != null)
+				{
+					socket.identifier = json.data.identifer;
+					machineSockets[socket.identifer] = socket;
+				}
+				machine.registration(json.data);
+			}else if(json.operation == "checkin")
+			{
+				machine.checkin(json.data);
+			}else{
+				return;//we don't want to send a response to the vending machine
 			}
 		}
 		else
